954b55ab4d05419aa732f49feae3e120c24ea37a
Anatoly Emelyanov <aemelyanov7@gmail.com>
2022-08-20 14:59:38 +0300

    Домашнее задание №4 сделано

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 25: Напишите цикл, который 
+// принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// Console.WriteLine("Введите число A");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i < 1; i++)
+// {
+
+//     Console.WriteLine("Результат равен " + Math.Pow(numberA, numberB));
+
+// }
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// String StringNumber = Convert.ToString(num); 
+// Char[] CharNumber = StringNumber.ToCharArray(); 
+// for (int i = 0; i < StringNumber.Length; i++)
+// {
+//     sum += Convert.ToInt32(StringNumber[i] - '0');  
+
+// }
+// Console.WriteLine($"Число {StringNumber}");
+// Console.WriteLine($"Сумма цифр числа равна: {sum}");
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] array = new int[8];
+
+// for (int i = 0; i < 8; i++)
+// {
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+
